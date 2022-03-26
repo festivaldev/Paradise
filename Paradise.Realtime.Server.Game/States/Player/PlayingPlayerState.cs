c7586646c03ed3658b909bc810e6c6cd3fd00110
SniperGER <6178765+SniperGER@users.noreply.github.com>
2022-03-26 23:32:52 +0100

    Automatically deplete overcharge

--- Paradise.Realtime.Server.Game/States/Player/PlayingPlayerState.cs
+++ Paradise.Realtime.Server.Game/States/Player/PlayingPlayerState.cs
@@ -1,5 +1,9 @@
-﻿namespace Paradise.Realtime.Server.Game {
+﻿using System;
+
+namespace Paradise.Realtime.Server.Game {
 	public class PlayingPlayerState : PlayerState {
+		private TimeSpan DepleteTime;
+
 		public PlayingPlayerState(GamePeer peer) : base(peer) { }
 
 		public override void OnEnter() {
@@ -10,6 +14,18 @@
 
 		public override void OnResume() { }
 
-		public override void OnUpdate() { }
+		public override void OnUpdate() {
+			if (DateTime.UtcNow.TimeOfDay >= DepleteTime) {
+				if (Peer.Actor.Info.Health > 100) {
+					Peer.Actor.Info.Health--;
+				}
+
+				if (Peer.Actor.Info.ArmorPoints > 100 && Peer.Actor.Info.ArmorPoints > Peer.Actor.Info.ArmorPointCapacity) {
+					Peer.Actor.Info.ArmorPoints--;
+				}
+
+				DepleteTime = DateTime.UtcNow.TimeOfDay.Add(TimeSpan.FromSeconds(1));
+			}
+		}
 	}
 }
