505ef11376e88013533aaa4fbc1b134ad1aa2fbe
SniperGER <6178765+SniperGER@users.noreply.github.com>
2023-09-19 23:07:07 +0200

    Allow both mouse x/y sensitivity to be set

---
+++ Client/Paradise.Client/Hooks/UserInputHook.cs
@@ -0,0 +1,41 @@
+﻿using HarmonyLib;
+using log4net;
+using UnityEngine;
+
+namespace Paradise.Client {
+	[HarmonyPatch(typeof(UserInput))]
+	public class UserInputHook {
+		private static readonly ILog Log = LogManager.GetLogger(nameof(UserInputHook));
+
+		static UserInputHook() {
+			Log.Info($"[{nameof(UserInputHook)}] hooking {nameof(UserInput)}");
+		}
+
+		[HarmonyPatch("UpdateMouse"), HarmonyPrefix]
+		public static bool UserInputHook_UpdateMouse_Prefix() {
+			if (Camera.main != null) {
+				float fovMult = Mathf.Pow(Camera.main.fieldOfView / ApplicationDataManager.ApplicationOptions.CameraFovMax, 1.1f);
+
+				UserInput.Mouse.x += AutoMonoBehaviour<InputManager>.Instance.RawValue(GameInputKey.HorizontalLook) * ApplicationDataManager.ApplicationOptions.InputXMouseSensitivity * fovMult;
+				UserInput.Mouse.x = ClampAngle(UserInput.Mouse.x, ApplicationDataManager.ApplicationOptions.InputMouseRotationMinX, ApplicationDataManager.ApplicationOptions.InputMouseRotationMaxX);
+
+				int mouseMult = (!ApplicationDataManager.ApplicationOptions.InputInvertMouse) ? 1 : -1;
+
+				UserInput.Mouse.y += AutoMonoBehaviour<InputManager>.Instance.RawValue(GameInputKey.VerticalLook) * ApplicationDataManager.ApplicationOptions.InputYMouseSensitivity * mouseMult * fovMult;
+				UserInput.Mouse.y = ClampAngle(UserInput.Mouse.y, ApplicationDataManager.ApplicationOptions.InputMouseRotationMinY, ApplicationDataManager.ApplicationOptions.InputMouseRotationMaxY);
+			}
+
+			return false;
+		}
+
+		private static float ClampAngle(float angle, float min, float max) {
+			if (angle < -360f) {
+				angle += 360f;
+			}
+			if (angle > 360f) {
+				angle -= 360f;
+			}
+			return Mathf.Clamp(angle, min, max);
+		}
+	}
+}
