dfb1cd53f4202a31454823f6409da6fa1d229b18
SniperGER <6178765+SniperGER@users.noreply.github.com>
2023-10-27 22:13:55 +0200

    Fixed bundle purchase process

--- Client/Paradise.Client/Hooks/BundleManagerHook.cs
+++ Client/Paradise.Client/Hooks/BundleManagerHook.cs
@@ -17,9 +17,18 @@ namespace Paradise.Client {
 			traverse = ParadiseTraverse.Create(typeof(BundleManager));
 		}
 
+		[HarmonyPatch("BuyBundle"), HarmonyPrefix]
+		public static bool BundleManager_BuyBundle_Prefix(BundleManager __instance, BundleUnityView bundle) {
+			if (traverse.Instance == null) {
+				traverse.Instance = __instance;
+			}
+
+			return true;
+		}
+
 		[HarmonyPatch("<BuyBundle>m__A7"), HarmonyPrefix]
 		public static bool BundleManager_BuyBundle_m__A7_Prefix(BundleManager __instance, bool success) {
-			traverse.InvokeMethod(__instance, "OnMicroTxnCallback", new object[] {
+			traverse.InvokeMethod("OnMicroTxnCallback", new object[] {
 				new Steamworks.MicroTxnAuthorizationResponse_t {
 					m_bAuthorized = (byte)(success ? 1 : 0)
 				}
