9250c435e00bac23e925a3e11a7734df24d7b576
SniperGER <6178765+SniperGER@users.noreply.github.com>
2023-10-31 00:22:17 +0100

    Improved Traverse

--- Client/Paradise.Client/ParadiseTraverse.cs
+++ Client/Paradise.Client/ParadiseTraverse.cs
@@ -25,7 +25,6 @@ namespace Paradise.Client {
 			Instance = instance;
 		}
 
-
 		public T GetField<T>(string fieldName) {
 			return traverse.Field<T>(fieldName).Value;
 		}
@@ -43,10 +42,35 @@ namespace Paradise.Client {
 		}
 
 		public object InvokeMethod(string methodName, params object[] parameters) {
-			return InvokeMethod(Instance, methodName, parameters);
+			return traverse.Method(methodName, parameters).GetValue();
 		}
 
-		public object InvokeMethod(object instance, string methodName, params object[] parameters) {
+
+		public static T GetField<T>(object instance, string fieldName) {
+			if (instance == null) throw new NullReferenceException("Instance cannot be null!");
+
+			return (T)AccessTools.Field(instance.GetType(), fieldName).GetValue(instance);
+		}
+
+		public static void SetField(object instance, string fieldName, object value) {
+			if (instance == null) throw new NullReferenceException("Instance cannot be null!");
+
+			AccessTools.Field(instance.GetType(), fieldName).SetValue(instance, value);
+		}
+
+		public static T GetProperty<T>(object instance, string propertyName) {
+			if (instance == null) throw new NullReferenceException("Instance cannot be null!");
+
+			return (T)AccessTools.Property(instance.GetType(), propertyName).GetValue(instance, null);
+		}
+
+		public static void SetProperty(object instance, string propertyName, object value) {
+			if (instance == null) throw new NullReferenceException("Instance cannot be null!");
+
+			AccessTools.Property(instance.GetType(), propertyName).SetValue(instance, value, null);
+		}
+
+		public static object InvokeMethod(object instance, string methodName, params object[] parameters) {
 			if (instance == null) throw new NullReferenceException("Instance cannot be null!");
 
 			return AccessTools.Method(instance.GetType(), methodName).Invoke(instance, parameters);
